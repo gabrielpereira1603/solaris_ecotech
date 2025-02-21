c317e37545a3ca7c359770d8502de206f44254cc
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2025-02-21 15:01:29 -0300

    enviando pacote de build

---
+++ resources/views/livewire/pages/home/home.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <x-slot name="header">
+        <h2 class="flex gap-1 items-center font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Propriedades') }}
+        </h2>
+    </x-slot>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
+use App\Livewire\Pages\Home\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+
+Route::get('/home', Home::class)->name('home');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::fallback(function () {
+    return redirect()->route('login');
+});
+
+
 require __DIR__.'/auth.php';
+
